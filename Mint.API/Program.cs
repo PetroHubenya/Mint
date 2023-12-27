6d1668ee437839e30603b37726c22c022e14af9e
Petro Hubenya <p.hubenya@gmail.com>
2023-12-27 12:47:10 -0500

    Services are registered in the Program.cs. First controller works fine.

--- Mint.API/Program.cs
+++ Mint.API/Program.cs
@@ -1,3 +1,8 @@
+using DataAccessLayer.Coingecko;
+using Interfaces.BusinessLogicLayer;
+using Interfaces.DataAccessLayer;
+using Mint.BLL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services
+builder.Services.AddScoped<ICoinService, CoinService>();
+builder.Services.AddScoped<IApiServiceCoingecko, ApiServiceCoingecko>();
 
 var app = builder.Build();
 
