2400b32ce81a589cd47823b0d605104d431d3b40
Petro Hubenya <p.hubenya@gmail.com>
2023-12-28 14:24:23 -0500

    ApiServiceCoincap.GetTopNCoinsAsync() method is written.

--- DataAccessLayer.Coingecko/ApiServiceCoincap.cs
+++ DataAccessLayer.Coingecko/ApiServiceCoincap.cs
@@ -3,13 +3,22 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.Coingecko
 {
     public class ApiServiceCoincap : IApiService
     {
+        private readonly HttpClient _httpClient;
+
+        public ApiServiceCoincap(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
         public Task<List<Coin>> GetAllCoinsAsync()
         {
             throw new NotImplementedException();
@@ -20,9 +29,45 @@ namespace DataAccessLayer.Coingecko
             throw new NotImplementedException();
         }
 
-        public Task<List<Coin>> GetTopNCoinsAsync(int limit)
-        {   
-            throw new NotImplementedException();
+        public async Task<List<Coin>> GetTopNCoinsAsync(int limit)
+        {
+            string apiUrl = $"https://api.coincap.io/v2/assets?limit={limit}";
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString = await response.Content.ReadAsStringAsync();
+
+                    if (string.IsNullOrEmpty(jsonString))
+                    {
+                        throw new Exception("Empty or null JSON string.");
+                    }
+                    else
+                    {
+                        List<Coin>? coins = JsonSerializer.Deserialize<List<Coin>>(jsonString);
+
+                        if (coins == null)
+                        {
+                            throw new Exception("Failed to deserialize JSON string.");
+                        }
+                        else
+                        {
+                            return coins;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new HttpRequestException($"Failed to retrieve coins. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
